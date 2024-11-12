ee0a2c0e1bcff2b2de6ddf84fb77a0f689841e88
hdoppelt <u0674744@umail.utah.edu>
2024-11-11 18:59:32 -0700

    Added GameLoop, DrawFrame

--- GUI/GUI.Client/Models/World.cs
+++ GUI/GUI.Client/Models/World.cs
@@ -1,6 +1,30 @@
-﻿namespace GUI.Client.Models
+﻿using System.Numerics;
+
+namespace GUI.Client.Models
 {
     public class World
     {
+
+        public Dictionary<int, Snake> Snakes;
+
+        public Dictionary<int, Food> Foods;
+
+        public int Size 
+        { get; private set; }
+
+        public World(int _size)
+        {
+            Snakes = new Dictionary<int, Snake>();
+            Foods = new Dictionary<int, Food>();
+            Size = _size;
+        }
+
+        public World(World world)
+        {
+            Snakes = new(world.Snakes);
+            Foods = new(world.Foods);
+            Size = world.Size;
+        }
+
     }
 }
