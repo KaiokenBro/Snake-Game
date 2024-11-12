99a9e72f40fad4c58a3e71eca78dc10645186fd8
KaiokenBro <victorvaldezlanda@gmail.com>
2024-11-11 19:32:16 -0700

    Added Point2D class.

--- GUI/GUI.Client/Models/Point2D.cs
+++ GUI/GUI.Client/Models/Point2D.cs
@@ -1,7 +1,42 @@
-﻿namespace GUI.Client.Models
+﻿using System.Text.Json.Serialization;
+
+namespace GUI.Client.Models
 {
+    /// <summary>
+    /// A class that represents a 2D point in space (just an X and Y pair).
+    /// </summary>
     public class Point2D
     {
+        /// <summary>
+        /// X-coordinate.
+        /// </summary>
+        [JsonInclude]
+        public int X { get; private set; }
+
+        /// <summary>
+        /// Y-coordinate.
+        /// </summary>
+        [JsonInclude]
+        public int Y { get; private set; }
+
+        /// <summary>
+        /// Constructor to initiate a Point2D object.
+        /// </summary>
+        /// <param name="X">X coordinate of the object</param>
+        /// <param name="Y">Y coordinate of the object</param>
+        public Point2D(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        /// <summary>
+        /// Empty constructor for Point2D.
+        /// </summary>
+        public Point2D()
+        {
+           //worry when need to use Json 
+        }
 
     }
 }
